6c875349083ca2eb059175f437d880eda1196fd8
Quemia <que.carol@hotmail.com>
2021-10-13 17:21:23 -0300

    Changed styles

--- src/App.js
+++ src/App.js
@@ -1,73 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Form from "./components/form";
+import "./App.css";
 function App() {
-  const [activities, setActivities] = useState([{"time_spent":"2", "activity":"Bike", "date":"2021-04-08"}, {"time_spent":"1", "activity":"Run", "date":"2021-04-07"}]);
-  const [totalHours, setTotalHours] = useState(3);
+  const [activities, setActivities] = useState([
+    { time_spent: "2", activity: "Bike", date: "2021-04-08" },
+    { time_spent: "1", activity: "Run", date: "2021-04-07" },
+  ]);
+  const [totalHours, setTotalHours] = useState("");
 
   const onSubmit = (event) => {
     event.preventDefault();
-    debugger
-    const form = event.currentTarget
-    const formData = new FormData(form)
-    const itemAttributes = Object.fromEntries(formData)
-    const newActivities = [...activities, itemAttributes]
-    const newHours = totalHours + Number(itemAttributes.time_spent)
-    
-    setActivities(newActivities)
-    setTotalHours(newHours)
-    
-    form.reset()
+    debugger;
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const itemAttributes = Object.fromEntries(formData);
+    const newActivities = [...activities, itemAttributes];
+    const newHours = totalHours + Number(itemAttributes.time_spent);
+
+    setActivities(newActivities);
+    setTotalHours(newHours);
+
+    form.reset();
   };
 
-  const removeActivity = (activity) =>{
-    const indexToRemove = activities.indexOf(activity)
-      if(indexToRemove !== -1){
-        const newActivities = [
-          ...activities.slice(0, indexToRemove),
-          ...activities.slice(indexToRemove + 1, activities.length)
-        ]
+  const removeActivity = (activity) => {
+    const indexToRemove = activities.indexOf(activity);
+    if (indexToRemove !== -1) {
+      const newActivities = [
+        ...activities.slice(0, indexToRemove),
+        ...activities.slice(indexToRemove + 1, activities.length),
+      ];
 
-        const newHours = totalHours - activity.time_spent
+      const newHours = totalHours - activity.time_spent;
 
-        setActivities(newActivities) 
-        setTotalHours(newHours)
-      } 
-  }
-
+      setActivities(newActivities);
+      setTotalHours(newHours);
+    }
+  };
 
   return (
-    <div>
-      <h1>Workout</h1>
+    <div className="Container">
+      <h1 className="TitlePage">Workout</h1>
       <Form onSubmit={onSubmit} />
       <table>
         <thead>
           <tr>
-            <td>Time</td>
-            <td>Type</td>
-            <td>Date</td>
-            <td>Actions</td>
+            <td className="tableType">Time</td>
+            <td className="tableType">Type</td>
+            <td className="tableType">Date</td>
+            <td className="tableType"> Actions</td>
           </tr>
         </thead>
         <tbody>
-          {activities.map((activity, index)=>(
-          <tr key={index}>
-            <td>{activity.time_spent}</td>
-            <td>{activity.activity}</td>
-            <td>{activity.date}</td>
-            <td>
-              <span onClick={() => removeActivity(activity)}>
-                X
-              </span>
-            </td>
-          </tr>
+          {activities.map((activity, index) => (
+            <tr key={index}>
+              <td className="tableType">{activity.time_spent}</td>
+              <td className="tableType">{activity.activity}</td>
+              <td className="tableType">{activity.date}</td>
+              <td className="tableType">
+                <span onClick={() => removeActivity(activity)}>X</span>
+              </td>
+            </tr>
           ))}
         </tbody>
       </table>
-      
-      <h1>{totalHours} Hour(s) of Exercises</h1>
 
+      <h1 className="totalHours">{totalHours} Hour(s) of Exercises</h1>
     </div>
-
   );
 }
 
--- src/components/form.js
+++ src/components/form.js
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from "react";
+import './form.css'
 
 const Form = (onSubmit) => {
+  return (
+    <div className="FormContainer">
+      <form onSubmit={onSubmit}>
+        <h2>Insert an item</h2>
+        <input type="number" name="time_spent" placeholder="hours of exercise" className="input"/>
+        <select name="activity" className="optionsForm">
+          <option value="">Choose an actvity</option>
+          <option value="Run">Run</option>
+          <option value="Bike">Bike</option>
+          <option value="Swimming">Swimming</option>
+        </select>
+        <input type="date" name="date" className="input"/>
 
+        <button type="submit" className="buttonForm">Add</button>
+      </form>
+    </div>
+  );
+};
 
-    return(
-    <form onSubmit={onSubmit}>
-    <h2>Insert an item</h2>
-    <input type="number" name="time_spent" />
-    <select name="activity">
-        <option value="">Choose an actvity</option>
-        <option value="Run">Run</option>
-        <option value="Bike">Bike</option>
-        <option value="Swimming">Swimming</option>
-    </select>
-    <input type="date" name="date" />
-
-    <button type="submit">Add</button>
-    </form>
-    )
-}
-
-export default Form
+export default Form;
